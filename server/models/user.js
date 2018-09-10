32dc3237e3ee70e04cc369d7d390195042d12ddc
Gajendar <gajendra.kumar@3pillarglobal.com>
2018-09-10 19:32:58 +0530

    Update the config to use

--- server/models/user.js
+++ server/models/user.js
@@ -56,7 +56,7 @@ UserSchema.methods.generarteAuthToken=function(){
  var token =jwt.sign({
      _id:user._id.toHexString(),
      access
-    },'abc123').toString();
+    },process.env.JWT_SECRET).toString();
 
     //user.tokens.push({access,token}); //old time
     user.tokens= user.tokens.concat([{access,token}]);
@@ -83,7 +83,7 @@ UserSchema.statics.findByToken =function(token){
 
  try{
   
-   decoded= jwt.verify(token,'abc123');
+   decoded= jwt.verify(token,process.env.JWT_SECRET);
 
  }catch(e){
 
